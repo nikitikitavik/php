f25462a277522aa37b0288dea7d6f6929c6c9e4e
Anton <shadoff1996@yandex.ru>
2024-05-05 17:43:28 +0300

    Test deploy

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test deploy v2</title>
+</head>
+<body>
+  <?php
+    include 'script.php';
+  ?>
+</body>
+</html>
